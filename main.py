f33889b3010aada7910484f1e29fbbbd5b041ca9
changr25 <changr25@mcmaster.ca>
2025-11-01 23:03:50 -0400

    working ish

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PIL import Image
 import cv2
 import re
 import phonenumbers
+from pdf2image import convert_from_path
+
 
 def get_email(s):
     result = []
@@ -50,14 +52,13 @@ def get_name(s):
 def locate_name_coords(name, img):
   boxes = pytesseract.image_to_boxes(img)
   boxes = boxes.splitlines()
-  
-  
   count = 0
   
   for i in range(len(boxes)):
     parts = boxes[i].split(" ")
     if boxes[i][0] == name[0][count]:
       count += 1
+      print("correct")
     
     if count > 4:
       x, y = int(parts[1]), int(parts[4])
@@ -66,12 +67,10 @@ def locate_name_coords(name, img):
   return x, y
   
 
-def read_ss():
-  image_file = "Tests/test3.png"
-  img_ori = PIL.Image.open(image_file)
+def read_ss(img_ori):
 
   width_ori, height_ori = img_ori.size
-  img = img_ori.crop((0, height_ori/8, width_ori/4, 800))
+  img = img_ori.crop((0, height_ori/8, width_ori/4, height_ori))
   text = pytesseract.image_to_string(img)
   
   #img.show()
@@ -86,10 +85,10 @@ def read_ss():
  
   width, height = img.size
 
-  x, y = locate_name_coords(name, img)
+  x, y = 220, 1375
 
   ##Locating Emails and Phone
-  img_cut1 = img.crop((0, height - y + 300, x + 200, height - y + 700))
+  img_cut1 = img.crop((0, height - y + (height - y) * 6, x * 3, int(height - y + x * 4.5)))
   text1 = pytesseract.image_to_string(img_cut1)
   
   clean1 = re.sub(r"[^\x00-\x7F]+", " ", text1)
@@ -99,25 +98,36 @@ def read_ss():
   emails = get_email(clean1)
 
   ##Locating Position
-  img_cut2 = img.crop((x - 50, height - y + 10, x + 250, height - y + 40))
-
+  img_cut2 = img.crop((int(0.7 * x), height - y + (height - y), x * 3, height - y + int(0.35 * x)))
   text2 = pytesseract.image_to_string(img_cut2)
   clean2 = re.sub(r"[^\x00-\x7F]+", " ", text2)
   position = re.sub(r"\s+", " ", clean2).strip()
 
   ##Getting Org
-  img_cut3 = img_ori.crop((x + 400, height - y + 100, x + 800, height - y + 160))
-  img_cut3.show()
+  img_cut3 = img_ori.crop((int(4 * x), height - y + (height - y) * 6, x * 6, height - y + int(1 * x)))
+
   text3 = pytesseract.image_to_string(img_cut3)
   clean3 = re.sub(r"[^\x00-\x7F]+", " ", text3)
   organization = re.sub(r"\s+", " ", clean3).strip()
  
-  print("Names:", name[0])
+  print("Name:", name[0])
   print("Phones: ", phones)
   print("Emails: ", emails)
   print("Postion: ", position)
   print("Organization:", organization)
+  print("\n")
 
-read_ss()
+def main():
+  pages = convert_from_path('first5.pdf', 500)
+  
+  for count, page in enumerate(pages):
+    top_img = pages[count].crop((500, 500, 3750, 2100))
+    read_ss(top_img)
+    bot_img = pages[count].crop((500, 2100, 3750, 3700))
+    read_ss(bot_img)
+
+  
+  
 
 
+main()
